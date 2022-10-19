bacdd196d9e014c978f456aeec971c01bc89bc71
Jai Sharma <87094729+jaisharma10@users.noreply.github.com>
2022-10-19 02:07:50 -0400

    functions

--- terramechanics1.py
+++ terramechanics1.py
@@ -4,11 +4,11 @@
 # GLobal Variables
 # =====================================================================
 
-# LRV values
+# LRV vehicle characteris
 b = 0.24        # wheel width (m)
 D = 0.81        # wheel diameter (m)
 m = 640         # vehicle mass (kg)
-g_moon = 1.625  # gravitational acc on moon (m/s^2) 
+num = 4         # number of wheels
 
 
 # soil characteristics
@@ -16,10 +16,11 @@ c1 = 170         # cohesive strength of soil (N/m^2)
 c2 = 0.017       # cohesive strength of soil (N/cm^2)
 cf = 0.05        # friction co.efficient
 rho = 2470       # soil density (N/m^3)
+g_moon = 1.625  # gravitational acc on moon (m/s^2) 
+
 
 phi = 33        # slope (deg)
 n = 1           # constant 1
-num = 4         # number of wheels
 
 # Additional Variables
 w = g_moon * m       # weight of vehivle on moon
